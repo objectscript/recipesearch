48a2c855107b9fdd7053158030ca53281b32b73d
ievgen.andrushchak <ievgen.andrushchak@globallogic.com>
2015-02-16 16:35:12 +0200

    added missed files

---
+++ RecipesSearch/RecipesSearch.CacheService/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipesSearch.CacheService.Models;
+using RecipesSearch.CacheService.Utils;
+using RecipesSearch.Data.Models;
+
+namespace RecipesSearch.CacheService.Services
+{
+    public class SearchService : CacheServiceBase
+    {
+        private const string Endpoint = "/iknow/search";
+
+        public List<SitePage> SearchByQuery(string query, int pageNumber, int pageSize)
+        {
+            var url = ServiceBase + Endpoint;
+            var parameters = new Dictionary<string, string>
+            {
+                {"query", Uri.EscapeDataString(query)},
+                {"startIndex", (pageNumber * pageSize).ToString()},
+                {"pageSize", pageSize.ToString()},
+            };
+
+            var respose = RestHelper.MakeRequest<SearchResponse>(url, RestHelper.HttpVerb.GET, parameters, null);
+
+            return respose.Children.ToList();
+        }
+    }
+}
