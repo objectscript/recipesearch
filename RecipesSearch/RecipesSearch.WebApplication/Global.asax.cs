0f20330e3cd8273989deba373c3fdc5580dc502b
killerDJO <killerDJO@gmail.com>
2015-05-10 19:08:35 +0300

    fixed login

--- RecipesSearch/RecipesSearch.WebApplication/Global.asax.cs
+++ RecipesSearch/RecipesSearch.WebApplication/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using InterSystems.AspNet.Identity.Cache;
 using RecipesSearch.BusinessServices.Logging;
 
 namespace RecipesSearch.WebApplication
@@ -17,6 +19,11 @@ namespace RecipesSearch.WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            using (var context = new IdentityDbContext("Cache"))
+            {
+                context.Database.Initialize(force: true);
+            }
 
             Logger.LogInfo("Application start.");
         }
