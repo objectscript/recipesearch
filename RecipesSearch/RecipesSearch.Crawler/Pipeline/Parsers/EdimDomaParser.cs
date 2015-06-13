757faf66767e1d7e2505d835cb97bf06143f89f7
killerDJO <killerDJO@gmail.com>
2015-06-13 22:51:46 +0300

    fixed parser

--- RecipesSearch/RecipesSearch.Crawler/Pipeline/Parsers/EdimDomaParser.cs
+++ RecipesSearch/RecipesSearch.Crawler/Pipeline/Parsers/EdimDomaParser.cs
@@ -11,6 +11,7 @@ namespace RecipesSearch.SitePagesImporter.Pipeline.Parsers
 
         public EdimDomaParser()
         {
+            IngredientsSelector = ".rec-ingredients .b-page_block__outside > p, .rec-ingredients .b-page_block__outside [itemprop=ingredients]";
             RecipeInstructionsSelector = "[itemprop=recipeInstructions] .b-page_block__content .b-list__clause__text";
             RecipeCategorySelector = ".l-centercol.utk-recipe .b-breadcrumbs p a:last";
             RatingSelector = "[itemprop=ratingValue]";
